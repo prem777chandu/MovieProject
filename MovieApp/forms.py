78ea7fb9e15c272146a8e179225b9ff3d95ebbae
prem777chandu <prem777chandu@gmail.com>
2022-11-05 10:37:01 +0530

    Initial commit

---
+++ MovieApp/forms.py
@@ -0,0 +1,21 @@
+from django import forms;
+from MovieApp.models import Movies
+
+class DateInput(forms.DateInput):
+    input_type = 'date';
+
+
+class MoviesForm(forms.ModelForm):
+    #no-separate fields are required(taken from model-Movies-class)
+    #class Meta:
+    #    model=Movies
+    #    fields='__all__'
+    releasedate = forms.DateField(widget=DateInput())
+    moviename = forms.CharField()
+    actor = forms.CharField()
+    actress = forms.CharField()
+    rating = forms.FloatField()
+
+    class Meta:
+        model = Movies
+        fields = ('releasedate', 'moviename', 'actor', 'actress', 'rating');
